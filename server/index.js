fba6437276faf2ce8026b46ef418dce78d5a72bb
Bon <bng22061996@gmail.com>
2018-02-01 01:09:36 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+var io = require('socket.io')(443);
+
+var arrUserInfo = [];
+
+io.on('connection', socket => {
+    socket.on('NGUOI_DUNG_DANG_KI', user => {
+        const isExist = arrUserInfo.some(e => e.ten === user.ten);
+        // xl disconnect
+        socket.peerId = user.peerId;
+        if (isExist) return socket.emit('DANG_KY_THAT_BAT');
+        arrUserInfo.push(user);
+        socket.emit('DANH_SACH_ONLINE', arrUserInfo);
+        socket.broadcast.emit('CO_NGUOI_DUNG_MOI', user);
+});
+
+    socket.on('disconnect', () => {
+        const index = arrUserInfo.findIndex(user => user.peerId === socket.peerId);
+        arrUserInfo.splice(index, 1);
+        io.emit("AI_DO_NGAT_KET_NOI", socket.peerId);
+    })
+});
